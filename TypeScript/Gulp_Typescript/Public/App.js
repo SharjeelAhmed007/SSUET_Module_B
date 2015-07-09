ca63cb52d56601f62a628c5ad3aee3e51dcbb8e1
SharjeelAhmed007 <sharjeelahmedt49@gmail.com>
2015-07-08 20:38:03 -0700

    javaScript file

---
+++ TypeScript/Gulp_Typescript/Public/App.js
@@ -0,0 +1,13 @@
+/*Created By Sharjeel Ahmed */
+var vehical = (function () {
+    function vehical() {
+    }
+    vehical.prototype.openDoor = function () { };
+    return vehical;
+})();
+var car = (function () {
+    function car() {
+    }
+    car.prototype.speed = function () { };
+    return car;
+})();
